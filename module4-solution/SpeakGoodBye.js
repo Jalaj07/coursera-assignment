49d20607add063bc2f2cff76e13c510828c9fa1c
jalaj <Jalajvarshney20@gmail.com>
2020-07-12 23:34:10 +0530

    module 4

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,8 +1,8 @@
-(function(window){
-	var byeSpeaker = {};		
-	byeSpeaker.speakWord = "Good Bye";
-	byeSpeaker.speak=function speak(names) {
-  	console.log(speakWord + " " +names);
-	}
-window.byeSpeaker = byeSpeaker;
-}) (window);
+(function(window) {
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
+})(window);
